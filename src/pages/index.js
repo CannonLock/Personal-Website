4c0d55aef46cc9055d937a790654c9ec8912a55c
CannonLock <lockannon@gmail.com>
2022-01-16 23:24:01 -0600

    Add in the project and blog queries

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react"
 import { Grid, Box, Paragraph, Heading, Image } from "theme-ui"
 import { graphql } from 'gatsby'
-import GIF from "../static/gifs/Breast-Stroke.gif"
 import Container from '../components/layout/Container'
 import jsx from "theme-ui"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout/Layout"
 import { Rounded } from "../components/Cards"
+import { Link } from "@theme-ui/components"
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const IndexPage = ({ data }) => {
 
-  const monstera = getImage(data.file)
+  const monstera = getImage(data.monstera)
+  const fern = getImage(data.fern)
+
+  const recent_project = data.recentProject.nodes[0]
+  const recent_blog = data.recentBlog.nodes[0]
 
   return (
     <Layout>
@@ -34,23 +39,39 @@ const IndexPage = ({ data }) => {
           <Box
             sx={{
               position: "relative",
-              width: ["300px", "500px"],
-              height: ["300px", "500px"]
+              width: ["300px", "450px"],
+              height: ["300px", "450px"]
             }}
           >
             <Box
               sx={{
                 position: "absolute",
-                p: 5,
-                zIndex: 99
+                height: "100%",
+                width: "100%",
+                zIndex: 99,
+                display: "flex",
               }}
             >
               <Rounded
                 sx={{
-                  p: 5
+                  ml: -6,
+                  mr: 4,
+                  my: 'auto',
+                  p: 3
                 }}
               >
-                This is some test text
+                <Heading sx={{display:'flex'}}>
+                  <Link
+                    href={"/blog/" + recent_blog.slug}
+                    to={"/blog/" + recent_blog.slug}
+                    sx={{color:"text", mx: 'auto'}}
+                  >
+                    {recent_blog.frontmatter.title}
+                  </Link>
+                </Heading>
+                <Paragraph sx={{pt:3}}>
+                  {recent_blog.frontmatter.excerpt}
+                </Paragraph>
               </Rounded>
             </Box>
             <Box>
@@ -58,7 +79,50 @@ const IndexPage = ({ data }) => {
             </Box>
           </Box>
         </Grid>
-
+        <Grid columns={['1fr', '2fr 1fr', 'auto 1fr']}>
+          <Box
+            sx={{
+              position: "relative",
+              width: ["350px", "550px"],
+              height: ["350px", "550px"]
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                height: "100%",
+                width: "100%",
+                zIndex: 99,
+                display: "flex",
+              }}
+            >
+              <Rounded
+                sx={{
+                  ml: 4,
+                  mr: -6,
+                  my: 'auto',
+                  p: 3
+                }}
+              >
+                <Heading sx={{display:'flex'}}>
+                  <Link
+                    href={"/blog/" + recent_project.slug}
+                    to={"/blog/" + recent_project.slug}
+                    sx={{color:"text", mx: 'auto'}}
+                  >
+                    {recent_project.frontmatter.title}
+                  </Link>
+                </Heading>
+                <Paragraph sx={{pt:3}}>
+                  {recent_project.frontmatter.excerpt}
+                </Paragraph>
+              </Rounded>
+            </Box>
+            <Box>
+              <GatsbyImage alt={"Fern Leaf"} image={fern} />
+            </Box>
+          </Box>
+        </Grid>
       </Container>
     </Layout>
   )
@@ -66,10 +130,46 @@ const IndexPage = ({ data }) => {
 
 export const pageQuery = graphql`
   query {
-    file(relativePath: {eq: "monstera.webp"}) {
+    fern: file(relativePath: {glob: "fern.png"}) {
       relativePath
       childImageSharp {
-        gatsbyImageData
+        gatsbyImageData(placeholder: TRACED_SVG)
+      }
+    }
+    monstera: file(relativePath: {glob: "*monstera.png"}) {
+      relativePath
+      childImageSharp {
+        gatsbyImageData(placeholder: TRACED_SVG)
+      }
+    }
+    recentBlog: allMdx(
+      sort: {fields: frontmatter___date, order: DESC}
+      filter: {fileAbsolutePath: {glob: "**/blog/**"}}
+      limit: 1
+    ) {
+      nodes {
+        id
+        slug
+        frontmatter {
+          date
+          github
+          title
+          excerpt
+        }
+      }
+    }
+    recentProject: allMdx(
+      sort: {fields: frontmatter___date, order: DESC}
+      filter: {fileAbsolutePath: {glob: "**/projects/**"}}
+    ) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "D-M-Y")
+          excerpt
+        }
+        id
+        slug
       }
     }
   }
